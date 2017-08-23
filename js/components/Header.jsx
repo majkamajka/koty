35e8291d6b214fbbb8b173e57845d2e69b479030
Majka Karpinska <maja.miri@gmail.com>
2017-08-23 02:01:26 +0200

    hamburgher menu icon animation and sliding menu - eslint fixes

--- js/components/Header.jsx
+++ js/components/Header.jsx
@@ -2,48 +2,24 @@ import React from 'react';
 import { Router, Route, Link, IndexLink, IndexRoute, hashHistory } from 'react-router';
 
 class Header extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      location: location
+      location: location,
     };
   }
 
-  scrollToElement = (elemId) => {
-    //event.preventDefault();
-
-    const myPromise = new Promise((resolve, reject) => {
-        resolve(hashHistory.push('/'));
-    });
-
-    myPromise.then(console.log(elemId))
-    .then(console.log(document.body))
-    .then(console.log(document.getElementById(elemId))) /// dlaczegoo null?! ;(
-    .then(document.getElementById(elemId).scrollIntoView())
-
-    //console.log(location);
-
-    //scrollIntoView());
-
-
-    // if (this.state.location.href === `${this.state.location.origin}/#/`) {
-    //   console.log("xxxx");
-    //   document.getElementById(elemId).scrollIntoView();
-    // }
-  }
-
   menuDisplay = () => {
-    const hamburgerMenu = document.getElementById("hamburger-menu");
-    hamburgerMenu.classList.toggle("inside");
-    hamburgerMenu.classList.toggle("outside");
+    const hamburgerMenu = document.getElementById('hamburger-menu');
+    hamburgerMenu.classList.toggle('inside');
+    hamburgerMenu.classList.toggle('outside');
   };
 
   hamburgerAnimate = () => {
-    const spans = document.querySelectorAll("#hamburger span");
-    spans[0].classList.toggle("rotate-upper");
-    spans[1].classList.toggle("hidden-span");
-    spans[2].classList.toggle("rotate-bottom");
+    const spans = document.querySelectorAll('#hamburger span');
+    spans[0].classList.toggle('rotate-upper');
+    spans[1].classList.toggle('hidden-span');
+    spans[2].classList.toggle('rotate-bottom');
   }
 
   onClick = () => {
@@ -51,17 +27,39 @@ class Header extends React.Component {
     this.hamburgerAnimate();
   }
 
+  scrollToElement = (elemId) => {
+    //event.preventDefault();
+
+    const myPromise = new Promise((resolve, reject) => {
+      resolve(hashHistory.push('/'));
+    });
+
+    myPromise.then(console.log(elemId))
+              .then(console.log(document.body))
+              .then(console.log(document.getElementById(elemId))) // dlaczegoo null?! ;(
+              .then(document.getElementById(elemId).scrollIntoView());
+
+    // console.log(location);
+
+    // scrollIntoView());
+
+    // if (this.state.location.href === `${this.state.location.origin}/#/`) {
+    //   console.log("xxxx");
+    //   document.getElementById(elemId).scrollIntoView();
+    // }
+  }
+
   render() {
     return (
       <div className="bar">
         <header className="row">
           <IndexLink to="/">
-            <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4 logo">
+            <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4 logo">
               <h1>Adopcje</h1>
             </div>
           </IndexLink>
 
-          <nav className="col-xs-8 col-sm-8 col-md-8 col-lg-8 menu">
+          <nav className="col-md-8 col-lg-8 menu">
             <ul id="menu-desktop">
               <IndexLink to="/adoptions">
                 <li>KOTY DO<br />ADOPCJI</li>
@@ -82,21 +80,19 @@ class Header extends React.Component {
 
           </nav>
           <div id="hamburger" onClick={this.onClick}>
-            <span></span>
-            <span></span>
-            <span></span>
+            <span />
+            <span />
+            <span />
           </div>
 
-            <ul id="hamburger-menu" className="outside">
-              <h1 className="heading">Menu</h1>
-              <IndexLink to="/adoptions"><li>Koty do adopcji</li></IndexLink>
-              <IndexLink to="/"><li onClick={() => this.scrollToElement('about')}>O Koterii</li></IndexLink>
-              <IndexLink to="/"><li onClick={() => this.scrollToElement('virtual-adoption')}>Wirtualna adopcja</li></IndexLink>
-              <IndexLink to="/"><li onClick={() => this.scrollToElement('other-help')}>Jak pomóc</li></IndexLink>
-              <IndexLink to="/"><li onClick={() => this.scrollToElement('contact')}>Kontakt</li></IndexLink>
-            </ul>
-
-
+          <ul id="hamburger-menu" className="outside">
+            <h1 className="heading">Menu</h1>
+            <IndexLink to="/adoptions"><li>Koty do adopcji</li></IndexLink>
+            <IndexLink to="/"><li onClick={() => this.scrollToElement('about')}>O Koterii</li></IndexLink>
+            <IndexLink to="/"><li onClick={() => this.scrollToElement('virtual-adoption')}>Wirtualna adopcja</li></IndexLink>
+            <IndexLink to="/"><li onClick={() => this.scrollToElement('other-help')}>Jak pomóc</li></IndexLink>
+            <IndexLink to="/"><li onClick={() => this.scrollToElement('contact')}>Kontakt</li></IndexLink>
+          </ul>
 
         </header>
 
